546320157d5107a4f07e86d56d2b2288c220766d
Thomas Armena <thomasarmena@gmail.com>
2018-07-06 17:00:52 -0400

    Set up konva-react

--- app/components/Wall.js
+++ app/components/Wall.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './Wall/wall.css';
-
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
 
 export default class Wall extends React.Component {
 	
 	render(){
 		return (
 			<div className="wall">
+				<Stage width={window.innerWidth} height={window.innerHeight}>
+					<Layer>
+						<Rect
+							x={300}
+							y={300}
+							width={100}
+							height={100}
+							fill={Konva.Util.getRandomColor()}
+							shadowBlur={5}
+						/>
+					</Layer>
+				</Stage>
 			</div>
 		);
 	}
