c832e2a5c81137ea0254e625cd87f6c07ac4f526
Thomas Armena <thomasarmena@gmail.com>
2018-07-09 16:11:43 -0400

    Populated side bar with selectors, made sidebar scrollable

--- app/components/SideBar.js
+++ app/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './SideBar/SideBar.css';
 import SlideButton from './SideBar/SlideButton';
+import Selector from './SideBar/Selector';
 export default class SideBar extends React.Component {
 	
 	constructor(props) {
@@ -18,8 +19,35 @@ export default class SideBar extends React.Component {
 	render() {
 		let slideclass = this.state.out? "sidebar-out" : "sidebar-in";
 		return (
+				
 			<div className={"sidebar "+slideclass}>
-				<SlideButton onClick={()=>this.slide()} />
+				<div className="sidebar-content">
+					<Selector />
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector></Selector>
+					<Selector />
+				</div>
+				<div className="sidebar-ext">
+					<SlideButton onClick={()=>this.slide()} />
+				</div>
 			</div>
 			
 		);
---
+++ app/components/SideBar/Selector.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SideBar.css';
+
+export default class Selector extends React.Component {
+	
+	render(){
+		return(
+			<div className="selector"></div>
+		);
+	}
+
+}
