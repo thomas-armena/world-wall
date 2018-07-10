3d4bf520415505a7ba950ad62f600b7dc4fa1178
Thomas Armena <thomasarmena@gmail.com>
2018-07-10 15:53:48 -0400

    Added dummy links to nav bar

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -6,8 +6,12 @@ export default class NavBar extends React.Component {
 	render() {
 		return(
 			<div className="nav-bar">
-
-				WORLD WALL
+				<h1> world wall </h1>
+				<ul>
+					<li><a href="">home</a></li>
+					<li><a href="">create</a></li>
+					<li><a href="">about</a></li>
+				</ul>
 			</div>
 		);
 	}	
