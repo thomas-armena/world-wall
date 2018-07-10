31cbe4a733a6cc988df2db6226b106e37af3d5bd
Thomas Armena <thomasarmena@gmail.com>
2018-07-10 17:06:33 -0400

    Routing different pages

--- app/components/App.js
+++ app/components/App.js
@@ -3,17 +3,54 @@ import SideBar from './SideBar';
 import Wall from './Wall';
 import NavBar from './NavBar';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 export default class App extends React.Component {
 	render() {
-		return (
+
+		const Editor = () => (
 			<div>
-				<NavBar />
-				<div id="editor-wrapper">
-					<SideBar />
-					<Wall />
-				</div>
+				<SideBar />
+				<Wall />
 			</div>
+		);
+
+		const Home = () => (
+			<div>Home</div>
+		);
+
+		const View = () => (
+			<div>View</div>
+		);
+
+		const About = () => (
+			<div>About</div>
+		);
+		return (
+			<Router>
+				<div>
+					<NavBar >
+						<h1> world wall </h1>
+						<ul>
+							<li><Link className="link" to="/">home</Link></li>
+							<li><Link className="link" to="/create">create</Link></li>
+							<li><Link className="link" to="/view">view</Link></li>
+							<li><Link className="link" to="/about">about</Link></li>
+						</ul>
+					</ NavBar>
+					<div id="content-wrapper">
+						<Route exact path="/" component={Home} />
+						<Route path="/create" component={Editor} />
+						<Route path="/view" component={View} />
+						<Route path="/about" component={About} />
+					</div>
+				</div>
+			</Router>
 		);
 
 	}
 }
+
+
+
+
+	
--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -2,16 +2,14 @@ import React from 'react';
 import './NavBar/NavBar.css';
 
 export default class NavBar extends React.Component {
-	
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return(
 			<div className="nav-bar">
-				<h1> world wall </h1>
-				<ul>
-					<li><a href="">home</a></li>
-					<li><a href="">create</a></li>
-					<li><a href="">about</a></li>
-				</ul>
+				{this.props.children}
 			</div>
 		);
 	}	
